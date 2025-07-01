8024060ee13633a00c11a312816bfec06ff79f6b
Kazumiii-arch <weskyboundrealms@gmail.com>
2025-07-02 00:10:13 +0800

    Update MysteryMerchant.java

--- src/main/java/com/yourpackage/mysterymerchant/MysteryMerchant.java
+++ src/main/java/com/yourpackage/mysterymerchant/MysteryMerchant.java
@@ -1,93 +1,64 @@
-package com.yourpackage.mysterymerchant;
+package com.yourpackage.mysterymerchant.api;
 
-import com.yourpackage.mysterymerchant.commands.CommandManager;
-import com.yourpackage.mysterymerchant.economy.EconomyManager;
-import com.yourpackage.mysterymerchant.listeners.PlayerChatListener;
-import com.yourpackage.mysterymerchant.listeners.PlayerInteractionListener;
-import com.yourpackage.mysterymerchant.merchant.MerchantManager;
-import org.bukkit.entity.Player;
-import org.bukkit.plugin.java.JavaPlugin;
+import com.yourpackage.mysterymerchant.MysteryMerchant;
+import com.yourpackage.mysterymerchant.merchant.Merchant;
+import me.clip.placeholderapi.expansion.PlaceholderExpansion;
+import org.bukkit.ChatColor;
+import org.bukkit.OfflinePlayer;
+import org.jetbrains.annotations.NotNull;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Objects;
-import java.util.UUID;
+public class PAPIExpansion extends PlaceholderExpansion {
 
-public final class MysteryMerchant extends JavaPlugin {
+    private final MysteryMerchant plugin;
 
-    private static MysteryMerchant instance;
-    private MerchantManager merchantManager;
-    private EconomyManager economyManager;
-    
-    // Maps to track what a player is currently editing via chat
-    private final Map<UUID, String> playerEditModeMap = new HashMap<>();
-    private final Map<UUID, Integer> playerEditingItemSlotMap = new HashMap<>();
+    public PAPIExpansion(MysteryMerchant plugin) {
+        this.plugin = plugin;
+    }
 
     @Override
-    public void onEnable() {
-        instance = this;
-        saveDefaultConfig();
+    public @NotNull String getIdentifier() {
+        return "mysterymerchant";
+    }
+
+    @Override
+    public @NotNull String getAuthor() {
+        return "YourName";
+    }
+
+    @Override
+    public @NotNull String getVersion() {
+        return plugin.getDescription().getVersion();
+    }
+
+    @Override
+    public boolean persist() {
+        return true; // This is required for the expansion to be registered
+    }
+
+    @Override
+    public String onRequest(OfflinePlayer player, @NotNull String params) {
+        if (params.equalsIgnoreCase("is_spawned")) {
+            boolean isSpawned = plugin.getMerchantManager().isMerchantActive();
+            return isSpawned ? "Yes" : "No";
+        }
+
+        if (params.equalsIgnoreCase("time_remaining")) {
+            Merchant merchant = plugin.getMerchantManager().getActiveMerchant();
+            if (merchant != null && merchant.isSpawned()) {
+                long remaining = merchant.getRemainingSeconds();
+                long minutes = remaining / 60;
+                long seconds = remaining % 60;
+                return String.format("%02d:%02d", minutes, seconds);
+            } else {
+                return "00:00";
+            }
+        }
         
-        // Initialize managers
-        economyManager = new EconomyManager(this);
-        merchantManager = new MerchantManager(this);
-
-        try {
-            Objects.requireNonNull(getCommand("mysterymerchant")).setExecutor(new CommandManager(this));
-        } catch (NullPointerException e) {
-            getLogger().severe("Could not register the 'mysterymerchant' command!");
-            getServer().getPluginManager().disablePlugin(this);
-            return;
+        if (params.equalsIgnoreCase("status")) {
+            boolean isSpawned = plugin.getMerchantManager().isMerchantActive();
+            return isSpawned ? ChatColor.GREEN + "Spawned" : ChatColor.RED + "Not Spawned";
         }
 
-        getServer().getPluginManager().registerEvents(new PlayerInteractionListener(this), this);
-        getServer().getPluginManager().registerEvents(new PlayerChatListener(this), this);
-        getLogger().info("MysteryMerchant has been enabled successfully!");
-    }
-
-    @Override
-    public void onDisable() {
-        if (merchantManager != null && merchantManager.isMerchantActive()) {
-            merchantManager.despawnMerchant();
-            getLogger().info("Mystery Merchant has been despawned due to server shutdown.");
-        }
-        saveConfig();
-        getLogger().info("MysteryMerchant has been disabled.");
-    }
-
-    // --- Methods to manage the player's editing state ---
-    public void setPlayerInEditMode(Player player, String editType, int itemSlot) {
-        playerEditModeMap.put(player.getUniqueId(), editType);
-        playerEditingItemSlotMap.put(player.getUniqueId(), itemSlot);
-    }
-
-    public void removePlayerFromEditMode(Player player) {
-        playerEditModeMap.remove(player.getUniqueId());
-        playerEditingItemSlotMap.remove(player.getUniqueId());
-    }
-
-    public String getPlayerEditMode(Player player) {
-        return playerEditModeMap.get(player.getUniqueId());
-    }
-    
-    public Integer getPlayerEditingItemSlot(Player player) {
-        return playerEditingItemSlotMap.get(player.getUniqueId());
-    }
-    
-    public boolean isPlayerInEditMode(Player player) {
-        return playerEditModeMap.containsKey(player.getUniqueId());
-    }
-
-    // --- Getters for main components ---
-    public static MysteryMerchant getInstance() {
-        return instance;
-    }
-
-    public MerchantManager getMerchantManager() {
-        return merchantManager;
-    }
-    
-    public EconomyManager getEconomyManager() {
-        return economyManager;
+        return null; // Placeholder not found
     }
 }
