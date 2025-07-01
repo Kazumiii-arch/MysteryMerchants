4778277a76a9382ba4054b102b4b0dca5494f244
Kazumiii-arch <weskyboundrealms@gmail.com>
2025-07-02 00:15:00 +0800

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,19 +6,23 @@ plugins {
 }
 
 group = "com.yourpackage"
-version = "1.2.0-SNAPSHOT" // Version reflects the addition of Tier 1 features
+version = "1.3.0-SNAPSHOT" // Version reflects the addition of Tier 2 & 3 features
 
 repositories {
     mavenCentral()
     maven { url = uri("https://repo.papermc.io/repository/maven-public/") }
     // Repository for the Vault API
     maven { url = uri("https://jitpack.io") }
+    // Repository for PlaceholderAPI
+    maven { url = uri("https://repo.extendedclip.com/content/repositories/placeholderapi/") }
 }
 
 dependencies {
     compileOnly("io.papermc.paper:paper-api:1.21-R0.1-SNAPSHOT")
     // The Vault API dependency
     compileOnly("com.github.MilkBowl:VaultAPI:1.7")
+    // The PlaceholderAPI dependency
+    compileOnly("me.clip:placeholderapi:2.11.6")
 }
 
 java {
